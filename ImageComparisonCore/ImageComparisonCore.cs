e193fae1ad711b2ceb676c5ef8b97b8379789cce
bdunbar <mail@bendunbar.co.uk>
2023-03-09 15:38:36 +0000

    update
    update code, and add better difference images

---
+++ ImageComparisonCore/ImageComparisonCore.cs
@@ -0,0 +1,38 @@
+﻿using ImageMagick;
+
+namespace ImageComparison.Core
+{
+    public class ImageComparisonCore
+    {
+        /// <summary>
+        /// Compare images
+        /// </summary>
+        /// <param name="baseImageLocation">Location of your base image you want to compare</param>
+        /// <param name="comparisonImageLocation">Location of the image you want to compare against</param>
+        /// <param name="differenceOutput">Where you want the difference saved</param>
+        /// <returns></returns>
+        public async Task<ImageComparisonResult> CompareImage(CompareImage comparison)
+        {
+            var baseImage = new MagickImage(comparison.BaseImage);
+            var comparisonImage = new MagickImage(comparison.ComparisonImage);
+            if (comparison.ImageMask != null)
+            {
+                var maskImage = new MagickImage(comparison.ImageMask);
+
+                baseImage.SetReadMask(maskImage);
+                comparisonImage.SetReadMask(maskImage);
+            }
+
+            using (var differenceImage = new MagickImage())
+            {
+                var result = baseImage.Compare(comparisonImage, ErrorMetric.Absolute, differenceImage);
+
+                return new ImageComparisonResult
+                {
+                    NoDifferences = result == 0,
+                    Differences = differenceImage.ToByteArray()
+                };
+            }
+        }
+    }
+}
